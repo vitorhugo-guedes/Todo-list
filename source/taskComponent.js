f949517b30dd554f9ba43bc8a234b39feb34d274
vitorhugo-guedes <vitoroliveira.guedes@outlook.com>
2021-08-19 14:46:21 -0300

    start

--- source/taskComponent.js
+++ source/taskComponent.js
@@ -1,5 +1,27 @@
 class Task extends HTMLElement{
     constructor(){
-        this.shadowRoot.
+        super()
+        this.makeTask()
+        
     }
+    makeTask(){
+        const shadow = this.attachShadow({mode: 'open'});
+        shadow.innerHTML = 'Olá';
+        shadow.appendChild(this.Styles());
+        return shadow
+    }
+    Styles(){
+        const style = document.createElement('style');
+        style.textContent = `
+            .algo{
+                background-color: pink;
+                width: 220px;
+                height: 200px; 
+            }
+        `
+        return style
+    }
+    
 }
+
+customElements.define('task-card', Task);
